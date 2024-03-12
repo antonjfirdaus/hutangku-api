ba2e542ac4b713d40fb484815ed086fa16399b50
J Firdaus <antonfirdaus64@gmail.com>
2024-03-12 17:42:17 +0700

    Update transaction.router.js

--- routes/transaction.router.js
+++ routes/transaction.router.js
@@ -5,7 +5,7 @@ const transactionController = require("../controller/transaction.controller")
 
 router.get("/", transactionController.getAll)
 router.get("/get-by-id/:id", transactionController.getById)
-router.post("/get-by-bucket-id/:id", transactionController.create)
+router.get("/get-by-bucket-id/:id", transactionController.getByBucketId)
 
 
 router.post("/create", transactionController.create)
